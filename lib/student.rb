6a2952651fd14e2c8732a866b77dda686e497b11
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-01-03 19:37:40 -0500

    commit tree 4b43d1552c1f40f31b70eae55bf5adb6f0c4ce31

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user'
 
-end
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(information)
+    self.knowledge << information
+  end
+
+ end
